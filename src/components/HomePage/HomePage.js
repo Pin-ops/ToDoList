04afb3e44b8ad13c11cf9fca68c45cab661f07f8
Pin-ops <pinozmen89@gmail.com>
2020-05-15 17:10:58 +0200

    second part

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
-import {ListGroup} from 'react-bootstrap'
+import { Accordion, Card, Button} from 'react-bootstrap'
 import InputsGroup from '../InputsGroup/InputsGroup'
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker-cssmodules.min.css'
 
 
 
@@ -8,6 +11,7 @@ class HomePage extends Component {
 
     state = {
             items: []
+
     }
 
     getItem = (e , date) => {
@@ -18,30 +22,59 @@ class HomePage extends Component {
         } )
     }
 
-
-
+    ExampleCustomInput = ({ onClick }) => (
+            <button className="example-custom-input" onClick={onClick}>
+               <span role="img"></span>📅
+            </button>
+    );
     render() {
+
         // console.log(this.state.items)
 
-        let newItems = this.state.items.map ( (val, i) =>
-            <ListGroup.Item
-                key={i}
-            >
-                {val}
-            </ListGroup.Item>
+        let addItemList = this.state.items.map ( (val, i) =>
+           <Card key={i}>
+                <Card.Header className="text-left">
+                    <DatePicker
+                        selected={this.state.startDate}
+                        onChange={date => this.setState ({ startDate : date} )
+                        }
+                        customInput={< this.ExampleCustomInput />}
+                    />
+                    <Accordion.Toggle as={Button} variant="link" eventKey={i}>
+                        {val}
+                    </Accordion.Toggle>
+                </Card.Header>
+
+                <Accordion.Collapse eventKey={i} >
+                    <Card.Body className="p-0" >
+
+                    <textarea
+                        rows="10"
+                        value={this.state.startDate}
+                        className="w-100 pl-5 pt-3 border border-0 "
+                        placeholder="Please give me detail ..."
+                        />
+                    </Card.Body>
+                </Accordion.Collapse>
+
+            </Card>
         )
 
 
         return (
             <div>
+
                 <InputsGroup
                     pHolder='Enter new item'
                     btnText='Add'
                     itemPipe={this.getItem}
                 />
-                <ListGroup>
-                    {newItems}
-                </ListGroup>
+
+                <Accordion>
+                    {addItemList}
+
+                </Accordion>
+
             </div>
                 )
     }
